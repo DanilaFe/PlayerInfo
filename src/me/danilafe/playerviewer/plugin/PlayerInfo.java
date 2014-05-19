639dfbfff919a8e8573c33c73b57452c9a8522e7
Danila Fedorin <nothingnut@gmail.com>
2014-05-18 17:25:03 -0700

    Added on-command method and first command - alert.

--- src/me/danilafe/playerviewer/plugin/PlayerInfo.java
+++ src/me/danilafe/playerviewer/plugin/PlayerInfo.java
@@ -1,11 +1,14 @@
 package me.danilafe.playerviewer.plugin;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 import me.danilafe.playerviewer.httpserver.HTTPServer;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -13,6 +16,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerInfo extends JavaPlugin{
 
+	public HashMap<String, String> alerts = new HashMap<String, String>();
 	public Logger logger = Logger.getLogger("minecraft");
 	public PluginDescriptionFile pdfile = this.getDescription();
 	public Configuration config = this.getConfig();
@@ -48,6 +52,28 @@ public class PlayerInfo extends JavaPlugin{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args){
+		if(sender instanceof Player){
+			Player p = (Player) sender;	
+			if(commandLabel.equalsIgnoreCase("checker")){
+				if(args.length > 0){
+					if(args[0].equalsIgnoreCase("alert")){
+						if(args.length > 2){
+							String alert = args[2];
+							for(int i = 3; i < args.length; i ++){
+								alert += " "+ args[i];
+							}
+							alerts.put(args[1], alert);
+						}
+					}
+				}
+			}
+		}
+
+		
+		return false;
 	}
 	
 
